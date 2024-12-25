350b5d8e72b26befecdda5721a1ddb19fc844da5
payton <994752084@qq.com>
2024-12-25 17:52:33 +0800

    12.25,重构了启动代码

--- start_by_docker.sh
+++ start_by_docker.sh
@@ -4,5 +4,5 @@ docker-compose -f  ./etcd-cluster/docker-compose.yml up -d
 echo "kong gateway building"
 docker-compose -f  ./gateway-kong/docker-compose.yml up -d
 echo "test service starting"
-go run ./cmd/test-service/main.go -d
+go run ./src/test-service/main.go -d
 
