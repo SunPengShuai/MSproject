f9ed9364b79e8ee7c87196f4292b767533e6c06b
payton <994752084@qq.com>
2025-01-17 17:45:24 +0800

    1.17

--- start_by_docker.sh
+++ start_by_docker.sh
@@ -2,6 +2,13 @@ echo "etcd cluster building"
 docker-compose -f  ./etcd-cluster/docker-compose.yml up -d
 echo "kong gateway building"
 docker-compose -f  ./gateway-kong/docker-compose.yml up -d
+echo "rabbitMq-cluster building"
+docker-compose -f  ./mq-rabbit/docker-compose.yml up -d
+echo "redis-cluster building"
+docker-compose -f  ./redis/docker-compose.yml up -d
+echo "mysql-cluster building"
+docker-compose -f  ./mysql-cluster/docker-compose.yml up -d
+
 echo "test service starting"
 go build -o ./src/test-service/test-service ./src/test-service/main.go
 ./src/test-service/test-service
